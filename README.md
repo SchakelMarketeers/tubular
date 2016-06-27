7a14f90a36122921cb50e80f9f96865ba9e9de90
Roelof Roos <github@roelof.io>
2016-06-27 13:13:46 +0200

    Major update

--- README.md
+++ README.md
@@ -1,4 +1,75 @@
-tubular
-=======
+# jQuery tubular
+[![MIT Licensed][ico-license]][license]
 
-jQuery tubular turns cat videos into website backgrounds.  It's that dangerous.
+Tubular is a jQuery plugin that lets you set a YouTube video as your page
+background.  Just attach it to your page wrapper element, set some options, and
+you're on your way.
+
+```js
+$('.my-wrapper-element').tubular({
+    // options
+});
+```
+
+## Tubular's *hello world*
+
+Assuming you're happy with the default options and you use a wrapper div with
+the id of *wrapper*, you simply attach to your wrapper div and specify the
+video you want to load (see below), and *Presto!* ... kittens in your website
+background!
+
+```js
+$('#wrapper').tubular({
+    videoId: '0Bmhjf0rKe8'
+});
+```
+
+## Options and defaults
+```js
+$('element').tubular({
+    ratio: 16/9 // usually either 4/3 or 16/9 -- tweak as needed
+    videoId: 'ZCAnLxRvNNc' // toy robot in space is a good default, no?
+    mute: true
+    repeat: true
+    width: $(window).width() // no need to override
+    wrapperZIndex: 99
+    playButtonClass: 'tubular-play'
+    pauseButtonClass: 'tubular-pause'
+    muteButtonClass: 'tubular-mute'
+    volumeUpClass: 'tubular-volume-up'
+    volumeDownClass: 'tubular-volume-down'
+    increaseVolumeBy: 10 // increment value; volume range is 1-100
+    start: 0 // starting position in seconds
+});
+```
+
+## A word of caution
+Tubular does not design your website for you. It works here thanks to alpha
+transparency on these gray boxes and the png logo on the top left. I built
+tubular thinking it would help experienced web designers and developers add
+some subtle background elements - emphasis on subtle - to their work. I'm sure
+there are some tasteful ways to use tubular and many, many more not so tasteful
+ways to use it. With great power comes great responsibility. The kitten example
+above is not to be taken seriously! :-)
+
+# Remember
+Tubular does one thing well. It takes a single video from YouTube at injects it
+as a full-screen background on a website. It scales the video, offsets the
+video and provides some basic controls for the video.  That's it.
+
+Tubular makes some assumptions on how your website works. First, it assumes you
+have a single wrapper element under the body tag that envelops all of your
+website content. It promotes that wrapper to z-index: 99 and position:
+relative. You can configure the z-index value in the options. So it's assuming
+your wrapper can accept positioning without breaking your site.
+
+Finally, tubular injects the YouTube video you specified as an iframe using
+fixed position at z-index: 1.  Browsers that do not support fixed positioning
+will not support tubular. Also, since the YouTube iframe API requires the HTML5
+postMessage feature, browsers that do not support it (I'm looking at you, IE7)
+will not support tubular - tubular will return false before any changes are
+made to your CSS in IE7.
+
+[license]: LICENSE
+[ico-license]: http://img.shields.io/github/license/schakelmarketeers/tubular.svg?style=flat
+
---
+++ src/js/tubular.js
@@ -0,0 +1,183 @@
+/**
+ * jQuery tubular plugin
+ * @author Sean McCambridge
+ * @see http://www.seanmccambridge.com/tubular
+ * @version 1.0
+ * @since 2010
+ * @license MIT License
+ */
+
+(function ($, window) {
+
+    'use strict';
+
+    // defaults
+    var defaults = {
+        ratio: 16/9, // usually either 4/3 or 16/9 -- tweak as needed
+        videoId: 'ZCAnLxRvNNc', // toy robot in space is a good default, no?
+        mute: true,
+        repeat: true,
+        width: $(window).width(),
+        wrapperZIndex: 99,
+        playButtonClass: 'tubular-play',
+        pauseButtonClass: 'tubular-pause',
+        muteButtonClass: 'tubular-mute',
+        volumeUpClass: 'tubular-volume-up',
+        volumeDownClass: 'tubular-volume-down',
+        increaseVolumeBy: 10,
+        start: 0,
+        end: false,
+        videoQuality: 'hd1080',
+        relatedVideos: 0
+    };
+
+    // methods
+
+    var tubular = function(node, options) { // should be called on the wrapper div
+        var player; // The Youtube player
+        var $node = $(node); // cache wrapper node
+
+        options = $.extend({}, defaults, options);
+
+        // build container
+        var tubularContainer = $('<div id="tubular-container" style="overflow: hidden; position: fixed; z-index: 1; width: 100%; height: 100%"><div id="tubular-player" style="position: absolute"></div></div><div id="tubular-shield" style="width: 100%; height: 100%; z-index: 2; position: absolute; left: 0; top: 0;"></div>');
+
+        // set up css prereq's, inject tubular container and set up wrapper defaults
+        tubularContainer.insertBefore($node);
+        $node.css({position: 'relative', 'z-index': options.wrapperZIndex});
+
+        // Resize function updates width, height and offset of player after
+        // resize/init
+        var resize = function() {
+            var width = $(window).width(),
+                pWidth, // player width, to be defined
+                height = $(window).height(),
+                pHeight, // player height, tbd
+                $tubularPlayer = $('#tubular-player');
+
+            // when screen aspect ratio differs from video, video must center
+            // and underlay one dimension
+
+            // if new video height < window height (gap underneath)
+            if (width / options.ratio < height) {
+                // get new player width
+                pWidth = Math.ceil(height * options.ratio);
+
+                // player width is greater, offset left; reset top
+                $tubularPlayer.width(pWidth).height(height).css({
+                    left: (width - pWidth) / 2,
+                    top: 0
+                });
+            } else { // new video width < window width (gap to right)
+
+                // get new player height
+                pHeight = Math.ceil(width / options.ratio);
+
+                // player height is greater, offset top; reset left
+                $tubularPlayer.width(width).height(pHeight).css({
+                    left: 0,
+                    top: (height - pHeight) / 2
+                });
+            }
+
+        };
+
+        // Handlers
+        var onPlayerReady = function(e) {
+            resize();
+            if (options.mute) {
+                e.target.mute();
+            }
+
+            e.target.seekTo(options.start);
+            e.target.playVideo();
+        };
+
+        var onPlayerStateChange = function(state) {
+            // if video ended and repeat option is set true
+            if (state.data === 0 && options.repeat) {
+                player.seekTo(options.start); // restart
+            }
+        };
+
+        // set up iframe player, use global scope so YT api can talk
+        window.player = window.player || {};
+        window.onYouTubeIframeAPIReady = function() {
+            player = new YT.Player('tubular-player', {
+                width: options.width,
+                height: Math.ceil(options.width / options.ratio),
+                videoId: options.videoId,
+                playerVars: {
+                    controls: 0,
+                    showinfo: 0,
+                    modestbranding: 1,
+                    iv_load_policy: 3,
+                    wmode: 'transparent',
+                    vq: options.videoQuality,
+                    rel: options.relatedVideos,
+                    end: options.end
+                },
+                events: {
+                    onReady: onPlayerReady,
+                    onStateChange: onPlayerStateChange
+                }
+            });
+        };
+
+        // events
+        $(window).on('resize.tubular', function() {
+            resize();
+        });
+
+        $('body').on('click','.' + options.playButtonClass, function(e) { // play button
+            e.preventDefault();
+            player.playVideo();
+        }).on('click', '.' + options.pauseButtonClass, function(e) { // pause button
+            e.preventDefault();
+            player.pauseVideo();
+        }).on('click', '.' + options.muteButtonClass, function(e) { // mute button
+            e.preventDefault();
+            if (player.isMuted()) {
+                player.unMute();
+            } else {
+                player.mute();
+            }
+        }).on('click', '.' + options.volumeDownClass, function(e) { // volume down button
+            e.preventDefault();
+            var currentVolume = player.getVolume();
+            if (currentVolume < options.increaseVolumeBy) {
+                currentVolume = options.increaseVolumeBy;
+            }
+            player.setVolume(currentVolume - options.increaseVolumeBy);
+        }).on('click', '.' + options.volumeUpClass, function(e) { // volume up button
+            e.preventDefault();
+            if (player.isMuted()) {
+                player.unMute(); // if mute is on, unmute
+            }
+            var currentVolume = player.getVolume();
+            if (currentVolume > 100 - options.increaseVolumeBy) {
+                currentVolume = 100 - options.increaseVolumeBy;
+            }
+            player.setVolume(currentVolume + options.increaseVolumeBy);
+        });
+    };
+
+    // load yt iframe js api
+
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // create plugin
+
+    $.fn.tubular = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'tubular_instantiated')) { // let's only run one
+                $.data(this, 'tubular_instantiated',
+                tubular(this, options));
+            }
+        });
+    };
+
+})(jQuery, window);
