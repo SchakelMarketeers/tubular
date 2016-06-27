3902e1ccc6a3fe9deb70735a26baa12f2b0561a9
Roelof Roos <github@roelof.io>
2016-06-27 15:59:57 +0200

    Fixed permission error

--- src/tubular.js
+++ src/tubular.js
@@ -209,8 +209,10 @@
     };
 
     // Load YT Iframe API (always via TLS)
-    $('<script src="https://www.youtube.com/iframe_api"></script>')
-        .appendTo('body');
+    var tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
     // Register plugin
     $.fn.tubular = function(options) {
